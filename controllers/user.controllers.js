9241b773cacb761e1812e8e81a7798ab46d69011
AdiTYa-JadHav77 <adityajadhav0502@gmail.com>
2022-01-11 13:11:33 +0530

    Test
    Testing the new branch

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -15,3 +15,39 @@ exports.read = (req, res) => {
       }
   });
 };
+
+
+// exports.updateName = (req, res) => {
+//     const {name}= req.body;
+//     User.findById(req.query.id)
+//     .then(response => {
+//         if (response.length == 0) {
+//             res.status(200).json({ message: "Enter the Name"})
+
+//         } else {
+//             const updatedName = new User({ name:name});
+//             updatedName.save().then(response => {
+//                     res.status(200).json({ message: "Name Updated successfully!" })
+//                 })
+//                 .catch(err => {
+//                     res.status(500).json({ error: err })
+//                 })
+//         }
+//     })
+//     .catch(err => {
+//         res.status(500).json({ error: err })
+//     })
+//   };
+  
+
+  exports.editNameOfStudent = (req, res) => {
+    User.updateOne({ _id: req.params.id }, { $push: { name: { $each: req.body.name } } }, (err, user) => {
+      if (response.length == 0) {
+        return res.status(200).json({ message: "Enter the Name"});
+      }
+      return res.json({
+        message: "Name edited successfully.!",
+        userData: user,
+      });
+    });
+  };
